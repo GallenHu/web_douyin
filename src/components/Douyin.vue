0865a26d732531c15ef160440af581f3a9a86457
GallenHu <huguiliang@zbj.com>
2018-05-31 18:50:46 +0800

    first commit

---
+++ src/components/Douyin.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <x-header>with right link<a slot="right">Feedback</a></x-header>
+    <video-player  class="video-player-box"
+                 ref="videoPlayer"
+                 :options="playerOptions"
+                 :playsinline="true"
+                 customEventName="customstatechangedeventname"
+
+                 @play="onPlayerPlay($event)"
+                 @pause="onPlayerPause($event)"
+                 @ended="onPlayerEnded($event)"
+                 @waiting="onPlayerWaiting($event)"
+                 @playing="onPlayerPlaying($event)"
+                 @loadeddata="onPlayerLoadeddata($event)"
+                 @timeupdate="onPlayerTimeupdate($event)"
+                 @canplay="onPlayerCanplay($event)"
+                 @canplaythrough="onPlayerCanplaythrough($event)"
+
+                 @statechanged="playerStateChanged($event)"
+                 @ready="playerReadied">
+  </video-player>
+  </div>
+
+</template>
+
+<script>
+/* eslint-disable */
+import { XHeader } from "vux";
+// eslint-disable-next-line
+import "video.js/dist/video-js.css";
+import { videoPlayer } from "vue-video-player";
+
+export default {
+  components: {
+    XHeader,
+    videoPlayer
+  },
+  data() {
+    return {
+      playerOptions: {
+        // videojs options
+        muted: true,
+        language: "en",
+        playbackRates: [0.7, 1.0, 1.5, 2.0],
+        sources: [
+          {
+            type: "video/mp4",
+            src:
+              "https://api.amemv.com/aweme/v1/play/?video_id=v0200f490000bbq1mkcm7fi9ml8eh8cg&line=0&ratio=720p&media_type=4&vr_type=0&test_cdn=None&improve_bitrate=0"
+          }
+        ],
+        poster: "/static/images/author.jpg"
+      }
+    };
+  },
+  mounted() {
+    console.log("this is current player instance object", this.player);
+  },
+  computed: {
+    player() {
+      return this.$refs.videoPlayer.player;
+    }
+  },
+  methods: {
+    // listen event
+    onPlayerPlay(player) {
+      // console.log('player play!', player)
+    },
+    onPlayerPause(player) {
+      // console.log('player pause!', player)
+    },
+    onPlayerEnded(player) {
+      // console.log('player ended!', player)
+    },
+    onPlayerLoadeddata(player) {
+      // console.log('player Loadeddata!', player)
+    },
+    onPlayerWaiting(player) {
+      // console.log('player Waiting!', player)
+    },
+    onPlayerPlaying(player) {
+      // console.log('player Playing!', player)
+    },
+    onPlayerTimeupdate(player) {
+      // console.log('player Timeupdate!', player.currentTime())
+    },
+    onPlayerCanplay(player) {
+      // console.log('player Canplay!', player)
+    },
+    onPlayerCanplaythrough(player) {
+      // console.log('player Canplaythrough!', player)
+    },
+    // or listen state event
+    playerStateChanged(playerCurrentState) {
+      // console.log('player current update state', playerCurrentState)
+    },
+    // player is ready
+    playerReadied(player) {
+      // seek to 10s
+      console.log("example player 1 readied", player);
+      player.currentTime(10);
+      // console.log('example 01: the player is readied', player)
+    }
+  }
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,32 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import FastClick from 'fastclick';
+import VueRouter from 'vue-router';
+import App from './App';
+// import Home from './components/HelloFromVux';
+import Douyin from './components/Douyin';
+
+Vue.use(VueRouter);
+
+const routes = [{
+  path: '/',
+  redirect: '/douyin',
+}, {
+  path: '/douyin',
+  component: Douyin,
+}];
+
+const router = new VueRouter({
+  routes,
+});
+
+FastClick.attach(document.body);
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app-box');
